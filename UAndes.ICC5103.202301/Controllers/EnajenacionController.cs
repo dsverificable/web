162bf59d5f27943055155ac6db939450cb129d82
JaMunioz <jamunoz6@miuandes.cl>
2023-03-19 13:17:54 -0300

    feat(*): initial framework

---
+++ UAndes.ICC5103.202301/Controllers/EnajenacionController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UAndes.ICC5103._202301.Models;
+
+namespace UAndes.ICC5103._202301.Controllers
+{
+    public class EnajenacionController : Controller
+    {
+        private InscripcionesBrDbEntities db = new InscripcionesBrDbEntities();
+
+        // GET: Enajenacion
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Enajenacion.ToListAsync());
+        }
+
+        // GET: Enajenacion/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enajenacion enajenacion = await db.Enajenacion.FindAsync(id);
+            if (enajenacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enajenacion);
+        }
+
+        // GET: Enajenacion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enajenacion/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id, CNE, Comuna, Manzana, Predio, RutEnajenante, PorcentajeEnajenante, CheckEnajenante, RutAdquiriente, PorcentajeAdquiriente, CheckAdquiriente, Fojas, FechaInscripcion, IdInscripcion.")] Enajenacion enajenacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enajenacion.Add(enajenacion);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(enajenacion);
+        }
+
+        // GET: Enajenacion/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enajenacion enajenacion = await db.Enajenacion.FindAsync(id);
+            if (enajenacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enajenacion);
+        }
+
+        // POST: Enajenacion/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id, CNE, Comuna, Manzana, Predio, RutEnajenante, PorcentajeEnajenante, CheckEnajenante, RutAdquiriente, PorcentajeAdquiriente, CheckAdquiriente, Fojas, FechaInscripcion, IdInscripcion.")] Enajenacion enajenacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enajenacion).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(enajenacion);
+        }
+
+        // GET: Enajenacion/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enajenacion enajenacion = await db.Enajenacion.FindAsync(id);
+            if (enajenacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enajenacion);
+        }
+
+        // POST: Enajenacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Enajenacion enajenacion = await db.Enajenacion.FindAsync(id);
+            db.Enajenacion.Remove(enajenacion);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
